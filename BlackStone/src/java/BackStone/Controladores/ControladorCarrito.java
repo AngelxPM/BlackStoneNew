c1a5098b008b94e3bd94122dae550f759aabe4c2
AlanGzz <alangeragzz@gmail.com>
2017-05-17 09:47:33 -0500

    Carrito

--- BlackStone/src/java/BackStone/Controladores/ControladorCarrito.java
+++ BlackStone/src/java/BackStone/Controladores/ControladorCarrito.java
@@ -1,12 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package BackStone.Controladores;
 
-import BlackStone.Modelos.ComentariosDAO;
-import BlackStone.Modelos.Producto;
+
+import BlackStone.Modelos.ProductoDAO;
+import BlackStone.Modelos.ProductoPOJO;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
@@ -16,6 +14,7 @@ import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,9 +22,13 @@ import javax.servlet.http.HttpSession;
 
 /**
  *
- * @author AngelxPM
+<<<<<<< HEAD
+ * @author alang
+ *//*
+ @Alan Gerardo Gonzalez Lopez
  */
 @WebServlet(name = "ControladorCarrito", urlPatterns = {"/ControladorCarrito"})
+
 public class ControladorCarrito extends HttpServlet {
 
     /**
@@ -36,29 +39,35 @@ public class ControladorCarrito extends HttpServlet {
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
+<<<<<<< HEAD
+     * @throws java.sql.SQLException
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
+        
+        HttpSession s = request.getSession();
+        int idProd = Integer.parseInt(request.getParameter("producto"));
+        String action = request.getParameter("action");
+        ProductoDAO pdao = new ProductoDAO();
+        
+        ProductoPOJO pojo = new ProductoPOJO();
+        
+        pojo.setProducto(idProd);
+        
+        if(action.equals("agregar")){
             
+            pdao.agregar(pojo);
             
-            HttpSession session = request.getSession();
-            RequestDispatcher dispatcher = null;
-            
-            dispatcher = request.getRequestDispatcher("Inicio.jsp");
-            
-            String accion = request.getParameter("accion");
-            
-            ComentariosDAO cDAO = new ComentariosDAO();
-            
-            ArrayList<Producto> Productos = cDAO.ProductoMore(accion);
-            session.setAttribute("Productos", Productos);
-            response.sendRedirect("Prueba.jsp"); 
-            
+            ArrayList <ProductoPOJO> productos = pdao.ver(pojo);
+            s.setAttribute("Productos", productos);
+            response.sendRedirect("Carrito.jsp");
             
         }
+
+    }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -91,11 +100,15 @@ public class ControladorCarrito extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+<<<<<<< HEAD
+        processRequest(request, response);
+=======
         try {
             processRequest(request, response);
         } catch (SQLException ex) {
             Logger.getLogger(ControladorCarrito.class.getName()).log(Level.SEVERE, null, ex);
         }
+>>>>>>> origin/master
     }
 
     /**
